3719a2594da05eba53911a4836152a6c093cd3d6
VadymBoyko <109297726+VadymBoyko@users.noreply.github.com>
2023-02-13 20:18:38 +0200

    refactoring

--- main.py
+++ main.py
@@ -16,6 +16,23 @@ def get_url(addition: str):
     return URL + addition
 
 
+def get_day_exchange(day_params):
+    return day_params['currency'], day_params['purchaseRate'], day_params['saleRate']
+
+
+def process_day_exchange(day_str, param):
+    result = dict()
+    day_dict = dict()
+    exchange = list(filter(lambda el: el['currency'] in ('EUR', 'USD'), param['exchangeRate']))
+    for i in exchange:
+        cur_name, cur_buy, cur_sale = get_day_exchange(i)
+        day_dict[cur_name] = dict()
+        day_dict[cur_name]['buy'] = cur_buy
+        day_dict[cur_name]['sale'] = cur_sale
+    result[day_str] = day_dict
+    return result
+
+
 async def main(day_cnt):
     exchange_result = []
     if day_cnt > 10:
@@ -23,19 +40,10 @@ async def main(day_cnt):
     async with aiohttp.ClientSession() as session:
         for day_n in range(day_cnt):
             day_str = get_day(day_n)
-            res = dict()
             try:
                 async with session.get(get_url(day_str)) as response:
                     if response.status == 200:
-                        day_dict = dict()
-                        result = await response.json()
-                        exchange = list(filter(lambda el: el['currency'] in ('EUR', 'USD'), result['exchangeRate']))
-                        for i in exchange:
-                            day_dict[i['currency']] = dict()
-                            day_dict[i['currency']]['buy'] = i['purchaseRate']
-                            day_dict[i['currency']]['sale'] = i['saleRate']
-                        res[day_str] = day_dict
-                        exchange_result.append(res)
+                        exchange_result.append(process_day_exchange(day_str, await response.json()))
                     else:
                         print(f"Error status: {response.status} for {get_url(day_str)}")
             except aiohttp.ClientConnectorError as err:
@@ -49,5 +57,5 @@ if __name__ == "__main__":
         days_cnt = int(sys.argv[1])
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main(days_cnt))
+    r = asyncio.run(main(3))
     print(r)
